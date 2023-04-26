f31817c5da3a9f1c9b167360422cf03154b13492
LamiaAlomairi <lama5alomairi@gmail.com>
2023-04-26 14:01:14 +0400

    Create Online_Education_Platform Project

---
+++ Online_Education_Platform/src/main/java/com/Online_Education_Platform/Online_Education_Platform/OnlineEducationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.Online_Education_Platform.Online_Education_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEducationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEducationPlatformApplication.class, args);
+	}
+
+}
---
+++ Online_Education_Platform/src/test/java/com/Online_Education_Platform/Online_Education_Platform/OnlineEducationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Online_Education_Platform.Online_Education_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEducationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
